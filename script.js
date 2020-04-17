35f5d3bbb9d1096d23d34bb14352110ca7d9f7d1
Tanner Nielsen <tan-ali5@outlook.com>
2020-04-17 14:56:00 -0700

    added var numbers/lowercase

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
