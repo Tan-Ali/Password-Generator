012bb96209953930cffae50f92442877e2f1fca8
Tanner Nielsen <tan-ali5@outlook.com>
2020-04-18 09:14:00 -0700

    fixed confimr lowerCase

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ function generatePassword() {
 
     else {
       var confirmNumbers = comfirm("Would you like numbers in your password?");
-      var comfirmLowerCase = confirm(Would you like lower case letters in your password?)
-      var comfirmUpperCase = confirm("Would you like upper case letters in your password?")
-      var comfirmSpecialCharacters = confirm("would you like special characters in your password?")    }
+      var comfirmLowerCase = confirm("Would you like lower case letters in your password?");
+      var comfirmUpperCase = confirm("Would you like upper case letters in your password?");
+      var comfirmSpecialCharacters = confirm("would you like special characters in your password?");    }
 }
 
 // Add event listener to generate button
