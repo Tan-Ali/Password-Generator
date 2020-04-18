ba7c4dbe7413c48bd2136ebdea41f41d257d5201
Tanner Nielsen <tan-ali5@outlook.com>
2020-04-18 08:53:47 -0700

    added function generatePassword and var passwordLength

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -14,6 +15,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+
+  var passwordLength = prompt("How many characters would you like your password to be? Password must contain between 8-128 characters.")
 }
 
 // Add event listener to generate button
