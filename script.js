1fc53d38222d20778ebdf85c24fd68ea13217a75
Tanner Nielsen <tan-ali5@outlook.com>
2020-04-18 19:21:41 -0700

    fixed wording

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", "<", ",", ".", ">", "/", "?"];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,6 +13,11 @@ function writePassword() {
 
 }
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "w", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'", "<", ",", ".", ">", "/", "?"];
+
 function generatePassword() {
 
   var passwordLength = prompt("How many characters would you like your password to be? Password must contain between 8-128 characters.")
@@ -24,15 +25,13 @@ function generatePassword() {
   if (!passwordLength) {
     alert("Please enter value");
     return;
-    }
-
+    } 
     else if (passwordLength < 8 || passwordLength > 128) {
       alert("Please enter a value between 8-128 characters.");
       return;
     }
-
     else {
-      var confirmNumbers = comfirm("Would you like numbers in your password?");
+      var confirmNumbers = confirm("Would you like numbers in your password?");
       var comfirmLowerCase = confirm("Would you like lower case letters in your password?");
       var comfirmUpperCase = confirm("Would you like upper case letters in your password?");
       var comfirmSpecialCharacters = confirm("Would you like special characters in your password?");
